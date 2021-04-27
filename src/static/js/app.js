51070a9f4f0b64b680d8b4de8e8e11c18bde79fd
sophieph <so.phouangsy@gmail.com>
2021-04-27 16:32:15 +0200

    modal image

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,10 +1,10 @@
 console.log('app.js')
 
 $( document ).ready(function() {
-   setImage() 
+   setImageWithoutBlur();
 });
 
-function setImage() {
+function setImageWithoutBlur() {
     let url = $('.movie-image');
 
     url.each(function() {
@@ -13,3 +13,20 @@ function setImage() {
         $(this).attr('src', str);
     });
 }
+
+$('.movie-image').on('click', function() {
+    console.log('modal');
+
+    let modal = $('#myModal');
+    let modalImg = $('#img01');
+    let img = $(this);
+
+    modal.css('display', 'block');
+    modalImg.attr('src', img.attr('src'));
+});
+
+$('.close').on('click', function() {
+    let modal = $('#myModal');
+    modal.css('display', 'none');
+});
+
