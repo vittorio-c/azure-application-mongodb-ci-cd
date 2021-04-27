8f6f309a18cff33656c41a1e58571e93abde38bf
Vittorio C <vittorio@chillemi.fr>
2021-04-27 13:31:01 +0200

    Azure pipeline build phase 6

--- deployscripts/deploy.sh
+++ deployscripts/deploy.sh
@@ -9,16 +9,13 @@ rm -rf ~/MongoDbCinema
 cp -r /VSTSAgent/_work/r1/a/_vittorio-c.azure-application-mongodb-ci-cd/movies-app-files/ ~/MongoDbCinema
 cd ~/MongoDbCinema
 
-deactivate
-python3 -m venv env
+python3.8 -m venv env
 source env/bin/activate
 
-pip install wheel
-pip install -r requirements.txt
+pip3.8 install wheel
+pip3.8 install -r requirements.txt
 EOF
 echo "Out"
 
 # # we restart server to take changes into account
 # sudo systemctl restart statsapp
-
-echo 'Salut' > /home/azureuser/salut.txt
