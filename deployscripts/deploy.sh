a53f57649a486d63a72e6ccd2aef736dd5342fa1
Vittorio C <vittorio@chillemi.fr>
2021-04-27 11:08:34 +0200

    Azure pipeline build phase 3

---
+++ deployscripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# sudo -i -u eltov bash << EOF
+# echo "In"
+# whoami
+
+# rm -rf ~/StatsApp
+
+# cp -r /VSTSAgent/_work/r1/a/_v-c-d.flask/drop/ ~/StatsApp
+# cd ~/StatsApp
+
+# python3 -m venv env
+# source env/bin/activate
+
+# pip install wheel
+# pip install -r requirements.txt
+# EOF
+# echo "Out"
+
+# # we restart server to take changes into account
+# sudo systemctl restart statsapp
+
+echo 'Salut' > /home/azureuser/salut.txt
