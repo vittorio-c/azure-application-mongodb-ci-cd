5abe9418851d3319df30e696987049e76642365e
Vittorio C <vittorio@chillemi.fr>
2021-04-29 09:38:07 +0200

    Remove env file

--- deployscripts/deploy.sh
+++ deployscripts/deploy.sh
@@ -4,14 +4,16 @@ sudo -i -u azureuser bash << EOF
 echo "In"
 whoami
 
+# Suppression de l'ancienne version de l'application
 rm -rf ~/MongoDbCinema
 
+# Copie de la nouvelle version depuis l'artefact crée lors du CI
 cp -r /VSTSAgent/_work/r1/a/_vittorio-c.azure-application-mongodb-ci-cd/movies-app-files/ ~/MongoDbCinema
 cd ~/MongoDbCinema
 
+# Setup de l'application
 python3.8 -m venv env
 source env/bin/activate
-
 pip3.8 install wheel
 pip3.8 install -r requirements.txt
 EOF
